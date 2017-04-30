e574eef741a2dfc310e4cb0831c789d616c9fd2b
gnappo1 <gnappo.1@live.it>
2017-04-29 21:44:24 -0700

    added #scrape_recipe to Scraper class

--- lib/grandma_sweets_cli_app/scraper.rb
+++ lib/grandma_sweets_cli_app/scraper.rb
@@ -8,4 +8,15 @@ class Scraper
     @recipes
   end
 
+  def scrape_recipes
+    Nokogiri::HTML(open("http://www.giallozafferano.it/ricette-cat/Dolci-e-Desserts/")).css("article").each do |r|
+      recipe = Recipe.new
+      recipe.name = r.css("h3 a").text
+      recipe.description = r.css("p").text
+      recipe.url = r.css("a").attr("href").value
+      @recipes << recipe
+    end
+  end
+
+
 end
