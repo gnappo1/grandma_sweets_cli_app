6473c1044b9270a13a6721a0665b763e8cf6141e
gnappo1 <gnappo.1@live.it>
2017-04-29 19:59:27 -0700

    First commit: included file grandma_sweets_cli_app in bin, modified .gitignore and Rakefile, created spec folder, .travis.yml and rspec files.

---
+++ lib/grandma_sweets_cli_app.rb
@@ -0,0 +1,5 @@
+require "grandma_sweets_cli_app/version"
+
+module GrandmaSweetsCliApp
+  # Your code goes here...
+end
---
+++ spec/grandma_sweets_cli_app_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe GrandmaSweetsCliApp do
+  it "has a version number" do
+    expect(GrandmaSweetsCliApp::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
